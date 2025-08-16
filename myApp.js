6b2674d446cf2efead714ac8b8c3e8bdfb178506
Jacob Ayinsakia <jacob.ayinsakia@hubtel.com>
2025-08-16 00:14:21 +0000

    Updated myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-
+require('dotenv').config()
 
 //challenge1
 //console.log("Hello World")
@@ -19,7 +19,11 @@ app.get("/", (req, res) => {
 
 
 app.get("/json",(req, res) =>{
-    res.json({"message": "Hello json"})
+    if ( process.env["MESSAGE_STYLE"] == "uppercase"){
+        res.json({"message": "HELLO JSON"})
+    } else {
+        res.json({"message": "Hello json"})
+    }
 })
 
 
